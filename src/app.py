2321a188bd4990301d1711a8999b39eb1bf039c6
Samuel Dushime <sdushime38@gmail.com>
2025-03-26 17:45:35 +0200

    d

--- src/app.py
+++ src/app.py
@@ -77,7 +77,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Load the trained model
-MODEL_PATH = "models\plant_disease_model.keras"
+MODEL_PATH = "models/plant_disease_model.h5"
 model = tf.keras.models.load_model(MODEL_PATH)
 
 # Define initial class names for plant diseases
